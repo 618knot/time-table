c3e593d0a8091b333af67474e0d64110c5a7d3bd
knot <82982400+618knot@users.noreply.github.com>
2022-06-09 14:15:43 +0900

    Update toCsv.py

--- toCsv.py
+++ toCsv.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import tabula
+import datetime
+gb = ['Go', 'Back']
+
+dtn = datetime.datetime.now()
+dtn = dtn.strftime('%Y-%m-%d')
 
 dfs = tabula.read_pdf("R４年度シャトルバス時刻表春学期_0606-0610.pdf", lattice=True, pages='all')
-
 for i in range(2):
-    dfs[i].to_csv(f"test{i}.csv", index=None)
+
+    dfs[i] = dfs[i].drop(['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2'], axis = 1)
+    dfs[i].to_csv(f"timeTable{gb[i]}_{dtn}.csv", index=None)
