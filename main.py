dd40fef2b42bee064baa1fd0c65d2379622c5372
618knot <knot618@gmail.com>
2022-08-14 21:30:52 +0900

    get/csv/　の条件分け

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from pydantic import BaseModel
 from pathlib import Path
@@ -5,27 +6,29 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 app = FastAPI()
 
-@app.get("/api/v1/get/csv/{filename:path}")
-async def go(filename: str):
-    current = Path()
-    file_path = current / "csv" / filename
-    now = datetime.now()
-    date = now.strftime("%Y-%m-%d")
-    
-    response = FileResponse(path = file_path, filename = f"{date}_{filename}")
-    return response
-
 class reqprop(BaseModel):
     content: str
     date: str
 
-
 @app.post("/api/v1/get/csv/")
 async def go(req: reqprop):
-    current = Path()
-    file_path = current / "csv" / req.content
-    now = datetime.now()
-    date = now.strftime("%Y-%m-%d")
+    items = ["goC.csv", "backS.csv"] 
+
+    if req.content in items:
+        current = Path()
+        file_path = current / "csv" / req.content
+        now = datetime.now()
+        date = now.strftime("%Y-%m-%d")
+        t = os.path.getctime(file_path)
+        generated_at = datetime.fromtimestamp(t)
+        generated_at = generated_at.strftime("%Y-%m-%d")
     
-    response = FileResponse(path = file_path, filename = f"{date}_{req.content}")
-    return response
+        if generated_at == req.date:
+            response = FileResponse(path = file_path, filename = f"{date}_{req.content}")
+            return response
+        elif generated_at != req.date:
+            #スクレイピング、ファイル生成
+            response = FileResponse(path = file_path, filename = f"{date}_{req.content}")
+            return response
+    else:
+        return {"message": "error: no such file"}
