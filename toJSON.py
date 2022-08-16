6e912a772e0db21140396975735066c952c211eb
618knot <knot618@gmail.com>
2022-08-16 19:02:14 +0900

    toJSONを追加

---
+++ toJSON.py
@@ -0,0 +1,13 @@
+def toJSON(csv_name):
+    import pandas as pd
+
+    df = pd.read_csv("csv/" + csv_name, header=0)
+
+    key_list = ["bus station"]
+    contents_list = [list(df.columns)]
+
+    for i in range(len(df)):
+        key_list.append(f"bus{i}")
+        contents_list.append(df.iloc[i].tolist())
+
+    return dict(zip(key_list, contents_list))
