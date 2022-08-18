31ae480ead2e4e6b39d7329bcadfea6622e56f76
618knot <knot618@gmail.com>
2022-08-18 22:04:55 +0900

    ファイル・フォルダ関係の生成処理 JSONに項目を追加するところを関数化

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from .toCsv import toCsv
+from getPdf import getPdf
+from toCsv import toCsv
 from toJSON import toJSON
 import os
 from datetime import datetime
 from pydantic import BaseModel
 from pathlib import Path
 from fastapi import FastAPI
+import glob
 app = FastAPI()
 
 class reqprop(BaseModel):
@@ -18,24 +20,35 @@ async def go(req: reqprop):
     if req.content in items:
         current = Path()
         file_path = current / "csv" / req.content
-        now = datetime.now()
-        date = now.strftime("%Y-%m-%d")
+
+        #downloadフォルダが空
+        if len(glob.glob("download/*")) == 0:
+            getPdf()
+        
+        #目的csvがない
+        if req.content not in glob.glob("csv/*"):
+            toCsv()
+
         t = os.path.getctime(file_path)
         generated_at = datetime.fromtimestamp(t)
         generated_at = generated_at.strftime("%Y-%m-%d")
     
         if generated_at == req.date:
-            response = toJSON(req.content)
-            response.setdefault("generated_at", generated_at)
-            response.setdefault("status", "saved")
+            response = append(toJSON(req.content),generated_at)
             return response
         elif generated_at != req.date:
             #スクレイピング、ファイル生成
+            getPdf()
             toCsv()
-            response = toJSON(req.content)
-            response.setdefault("generated_at", date)
-            response.setdefault("status", "new")
+            response = append(toJSON(req.content),generated_at)
             return response
 
     else:
         return {"message": "error: no such file"}
+
+def append(response: dict, generated_at):
+    pdf = glob.glob("download/*")
+    response.setdefault("source", pdf[0])
+    response.setdefault("generated at", generated_at)
+    response.setdefault("status", "saved csv")
+    return response
